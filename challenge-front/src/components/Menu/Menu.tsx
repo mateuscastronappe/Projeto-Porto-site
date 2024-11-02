84cf3336895e6f6a4ef53fdb32ca8198beca729f
mateuscastronappe <teteucastro31@gmail.com>
2024-11-01 22:32:16 -0300

    arrumando a rota e o nome da li para planos

--- challenge-front/src/components/Menu/Menu.tsx
+++ challenge-front/src/components/Menu/Menu.tsx
@@ -7,7 +7,7 @@ export default function Menu() {
                 <li><Link href="/">Home</Link></li>
                 <li><Link href="/pages/ajuda">Ajuda</Link></li>
                 <li><Link href="/pages/sobre-nos">Sobre nós</Link></li>
-                <li><Link href="/">Home</Link></li>
+                <li><Link href="/pages/planos">Planos</Link></li>
                 <li><Link href="/pages/integrantes">Integrantes</Link></li>
             </ul>
         </nav>
