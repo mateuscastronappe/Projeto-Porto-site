f9def14ae774140b95375929a8c6147fe1515ec6
mateuscastronappe <teteucastro31@gmail.com>
2024-11-01 21:51:53 -0300

    estilizando o cabecalho

--- challenge-front/src/components/Cabecalho/Cabecalho.tsx
+++ challenge-front/src/components/Cabecalho/Cabecalho.tsx
@@ -4,8 +4,9 @@ import imgLogo from "@/img/porto-logo.png";
 
 export default function Cabecalho() {
     return (
-        <header className="text-black inline-flex items-center mx-auto pl-[20%] h-36 w-full max-w-screen-xl">
-            <Image src={imgLogo} alt="logo" width={100} height={100} />
+        <header className="cabecalho text-black inline-flex items-center mx-auto pl-[20%] h-36 w-full max-w-screen-xl">
+            <Image src={imgLogo} alt="logo" width={40} height={40} className="mr-2 mt-2 inline-flex" />
+            <h1 className="mr-10 text-3xl font-bold">Porto</h1>
             <Menu />
         </header>
     );
