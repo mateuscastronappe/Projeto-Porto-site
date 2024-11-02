3e637dec31aaefc5f400728e58899377f485d3b8
mateuscastronappe <teteucastro31@gmail.com>
2024-11-02 18:55:53 -0300

    adicionando conteudo e estilizandoa  página de duvidas

--- challenge-front/src/app/pages/ajuda/page.tsx
+++ challenge-front/src/app/pages/ajuda/page.tsx
@@ -1,8 +1,14 @@
-export default function Ajuda() {
+"use client"
+import PerguntaForm from '@/components/Pergunta/Pergunta';
 
+const Ajuda = () => {
     return (
-      <div>
-        <h1>Ajuda</h1>
-      </div>
+      <div className="bg-white p-5 rounded-lg w-1/2 ml-[20%] mt-[2%] shadow-[0_0_10px_rgba(0,35,200,0.4)]">
+            <h1 className="text-[#00369b] text-left text-xl">FAQ - DÚVIDAS FREQUÊNTES</h1>
+            <PerguntaForm />
+        </div>
     );
-  }
+};
+
+export default Ajuda;
+
