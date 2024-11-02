1aa21a8200873af570d56b87b7c954f1ff5048aa
mateuscastronappe <teteucastro31@gmail.com>
2024-11-02 20:36:06 -0300

    adicionando conteudo e estilizando a página

--- challenge-front/src/app/pages/planos/page.tsx
+++ challenge-front/src/app/pages/planos/page.tsx
@@ -1,8 +1,69 @@
-export default function Planos() {
+"use client"
+import PlanoCard from '@/components/Cards/Plano'; 
+import { selectPlan } from '@/utils/pagamentoPlanosUtils';
+import { useState } from 'react';
 
-    return (
+const PlanosEPrecos = () => {
+  const [selectedPlan, setSelectedPlan] = useState<string | null>(null);
+  const [showPaymentSection, setShowPaymentSection] = useState<boolean>(false);
+
+  const handleSelectPlan = (plan: string) => {
+    selectPlan(plan, setSelectedPlan, setShowPaymentSection);
+  };
+  return (
+    <>
       <div>
-        <h1>Planos</h1>
+        <div className="grid grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-5 mt-[3%]">
+          <div className="text-center">
+            <h1 className="bg-gradient-to-l from-[#67caf8] via-[#3c78d3] to-[#67caf8] text-transparent bg-clip-text text-[40px]">
+              Escolha o Plano Ideal Para Você
+              </h1>
+            
+            {/* Silver Plan */}
+            <PlanoCard
+              id="silver-plan"
+              title="Silver"
+              benefits={[
+                'Anúncios',
+                'Acesso aos Mecânicos',
+                'Consultoria em dias úteis'
+              ]}
+              price="19,90"
+              onSelectPlan={() => handleSelectPlan('silver')}
+            />
+            
+            {/* Gold Plan */}
+            <PlanoCard
+              id="gold-plan"
+              title="Gold"
+              benefits={[
+                'Sem Anúncios',
+                'Acesso aos Mecânicos',
+                'Consultoria 24h',
+                'Desconto em peças'
+              ]}
+              price="29,90"
+              onSelectPlan={() => handleSelectPlan('gold')}
+            />
+            
+            {/* Diamond Plan */}
+            <PlanoCard
+              id="diamond-plan"
+              title="Diamond"
+              benefits={[
+                'Sem Anúncios',
+                'Acesso aos Mecânicos',
+                'Consultoria 24h',
+                'Fale com Fornecedores'
+              ]}
+              price="39,90"
+              onSelectPlan={() => handleSelectPlan('diamond')}
+            />
+          </div>
+        </div>
       </div>
-    );
-  }
+    </>
+  );
+};
+
+export default PlanosEPrecos;
