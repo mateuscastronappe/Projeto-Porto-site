fc536271a212d48f78c9836485536d7ecda6d6b5
mateuscastronappe <teteucastro31@gmail.com>
2024-11-02 21:30:10 -0300

    tirando o css do cabecalho e colocando no global.css

--- challenge-front/src/components/Cabecalho/Cabecalho.tsx
+++ challenge-front/src/components/Cabecalho/Cabecalho.tsx
@@ -4,9 +4,9 @@ import imgLogo from "@/img/porto-logo.png";
 
 export default function Cabecalho() {
     return (
-        <header className="cabecalho text-black inline-flex items-center mx-auto pl-[20%] h-36 w-full max-w-screen-xl">
+        <header className="cabecalho">
             <Image src={imgLogo} alt="logo" width={40} height={40} className="mr-2 mt-2 inline-flex" />
-            <h1 className="mr-10 text-3xl font-bold">Porto</h1>
+            <h1>Porto</h1>
             <Menu />
         </header>
     );
