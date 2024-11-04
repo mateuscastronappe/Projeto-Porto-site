209c1d6395f18c68b1efb771f68567cb25f59c9f
mateuscastronappe <teteucastro31@gmail.com>
2024-11-03 22:31:40 -0300

    adicionando o validadro na a´rea de login,verificando se está cadastrado e direcionando para o home

--- challenge-front/src/app/pages/login/page.tsx
+++ challenge-front/src/app/pages/login/page.tsx
@@ -2,22 +2,37 @@
 import Link from 'next/link';
 import { validarCPF, validarCNPJ } from '@/utils/validador'; 
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Login() {
   const [username, setUsername] = useState<string>('');
   const [rememberMe, setRememberMe] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
+  const router = useRouter();
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const cpfOuCnpj = username.replace(/[^\d]+/g, '');
 
-    if (cpfOuCnpj.length === 11 && validarCPF(cpfOuCnpj)) {
-      console.log('CPF válido:', cpfOuCnpj);
-    } else if (cpfOuCnpj.length === 14 && validarCNPJ(cpfOuCnpj)) {
-      console.log('CNPJ válido:', cpfOuCnpj);
+    const storedCpf = localStorage.getItem('cpf');
+    const storedCnpj = localStorage.getItem('cnpj');
+
+    
+    const isValidCPF = validarCPF(cpfOuCnpj);
+    const isValidCNPJ = validarCNPJ(cpfOuCnpj);
+
+    if ((cpfOuCnpj === storedCpf && isValidCPF) || (cpfOuCnpj === storedCnpj && isValidCNPJ)) {
+      setSuccessMessage('Login bem-sucedido!');
+      setError(null);
+      
+      setUsername('');
+      setTimeout(() => {
+        router.push('/'); 
+      }, 2000);
     } else {
-      setError('Número inválido');
+      setError('Número inválido ou não cadastrado');
+      setSuccessMessage(null);
     }
   };
 
@@ -49,6 +64,7 @@ export default function Login() {
               <label htmlFor="rememberMe">Lembrar-me</label>
             </div>
             {error && <p style={{ color: 'red' }}>{error}</p>}
+            {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
             <p>
               Não tem uma conta?{' '}
               <Link href="/pages/cadastro">
