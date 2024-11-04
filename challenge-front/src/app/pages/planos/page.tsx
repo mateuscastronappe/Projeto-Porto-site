61c042e4a42a3f76bf54fa0b9652c0834ae5ad55
mateuscastronappe <teteucastro31@gmail.com>
2024-11-03 22:33:54 -0300

    ignorando o selectedPlan e showPaymentSection para o deploy na vercel

--- challenge-front/src/app/pages/planos/page.tsx
+++ challenge-front/src/app/pages/planos/page.tsx
@@ -1,65 +1,47 @@
-"use client"
-import PlanoCard from '@/components/Cards/Plano'; 
+"use client";
+import PlanoCard from '@/components/Cards/Plano';
 import { selectPlan } from '@/utils/pagamentoPlanosUtils';
 import { useState } from 'react';
 
 const PlanosEPrecos = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [selectedPlan, setSelectedPlan] = useState<string | null>(null);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [showPaymentSection, setShowPaymentSection] = useState<boolean>(false);
 
   const handleSelectPlan = (plan: string) => {
     selectPlan(plan, setSelectedPlan, setShowPaymentSection);
   };
+
   return (
-    <>
-      <div>
-        <div className="containerPlanos">
-          <div className="planos">
-            <h1 className="titulo-escolha">Escolha o Plano Ideal Para Você</h1>
-            {/* Silver Plan */}
-            <PlanoCard
-              id="silver-plan"
-              title="Silver"
-              benefits={[
-                'Anúncios',
-                'Acesso aos Mecânicos',
-                'Consultoria em dias úteis'
-              ]}
-              price="19,90"
-              onSelectPlan={() => handleSelectPlan('silver')}
-            />
-            
-            {/* Gold Plan */}
-            <PlanoCard
-              id="gold-plan"
-              title="Gold"
-              benefits={[
-                'Sem Anúncios',
-                'Acesso aos Mecânicos',
-                'Consultoria 24h',
-                'Desconto em peças'
-              ]}
-              price="29,90"
-              onSelectPlan={() => handleSelectPlan('gold')}
-            />
-            
-            {/* Diamond Plan */}
-            <PlanoCard
-              id="diamond-plan"
-              title="Diamond"
-              benefits={[
-                'Sem Anúncios',
-                'Acesso aos Mecânicos',
-                'Consultoria 24h',
-                'Fale com Fornecedores'
-              ]}
-              price="39,90"
-              onSelectPlan={() => handleSelectPlan('diamond')}
-            />
-          </div>
+    <div>
+      <div className="containerPlanos">
+        <div className="planos">
+          <h1 className="titulo-escolha">Escolha o Plano Ideal Para Você</h1>
+          <PlanoCard
+            id="silver-plan"
+            title="Silver"
+            benefits={['Anúncios', 'Acesso aos Mecânicos', 'Consultoria em dias úteis']}
+            price="19,90"
+            onSelectPlan={() => handleSelectPlan('silver')}
+          />
+          <PlanoCard
+            id="gold-plan"
+            title="Gold"
+            benefits={['Sem Anúncios', 'Acesso aos Mecânicos', 'Consultoria 24h', 'Desconto em peças']}
+            price="29,90"
+            onSelectPlan={() => handleSelectPlan('gold')}
+          />
+          <PlanoCard
+            id="diamond-plan"
+            title="Diamond"
+            benefits={['Sem Anúncios', 'Acesso aos Mecânicos', 'Consultoria 24h', 'Fale com Fornecedores']}
+            price="39,90"
+            onSelectPlan={() => handleSelectPlan('diamond')}
+          />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
