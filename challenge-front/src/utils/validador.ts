1da4d2bab8235880da74dbde9caa75f7ed401c67
mateuscastronappe <teteucastro31@gmail.com>
2024-11-03 22:30:08 -0300

    atualizando a forma de validar cpf e cnpj

--- challenge-front/src/utils/validador.ts
+++ challenge-front/src/utils/validador.ts
@@ -1,66 +1,46 @@
+const CPF_LENGTH = 11;
+const CNPJ_LENGTH = 14;
+
+const isRepeatedDigits = (num: string): boolean => /^(\d)\1{10}$/.test(num);
+
+const calculateDigit = (numbers: string, weight: number): number => {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += parseInt(numbers.charAt(i)) * weight--;
+        if (weight < 2) weight = 9;
+    }
+    return sum % 11 < 2 ? 0 : 11 - (sum % 11);
+};
+
 export const validarCPF = (cpf: string): boolean => {
     cpf = cpf.replace(/[^\d]+/g, '');
-    if (cpf.length !== 11 || /^(\d)\1{10}$/.test(cpf)) return false;
+    if (cpf.length !== CPF_LENGTH || isRepeatedDigits(cpf)) return false;
 
-    let soma = 0;
-    let resto: number;
-    for (let i = 1; i <= 9; i++) {
-        soma += parseInt(cpf.charAt(i - 1)) * (11 - i);
-    }
-    resto = (soma * 10) % 11;
-    if (resto === 10 || resto === 11) resto = 0;
-    if (resto !== parseInt(cpf.charAt(9))) return false;
+    const firstDigit = calculateDigit(cpf.substring(0, 9), 10);
+    if (firstDigit !== parseInt(cpf.charAt(9))) return false;
 
-    soma = 0;
-    for (let i = 1; i <= 10; i++) {
-        soma += parseInt(cpf.charAt(i - 1)) * (12 - i);
-    }
-    resto = (soma * 10) % 11;
-    if (resto === 10 || resto === 11) resto = 0;
-    if (resto !== parseInt(cpf.charAt(10))) return false;
-
-    return true;
+    const secondDigit = calculateDigit(cpf.substring(0, 10), 11);
+    return secondDigit === parseInt(cpf.charAt(10));
 };
 
 export const validarCNPJ = (cnpj: string): boolean => {
     cnpj = cnpj.replace(/[^\d]+/g, '');
-    if (cnpj.length !== 14) return false;
+    if (cnpj.length !== CNPJ_LENGTH) return false;
 
-    let tamanho = cnpj.length - 2;
-    let numeros = cnpj.substring(0, tamanho);
-    let digitos = cnpj.substring(tamanho);
-    let soma = 0;
-    let pos = tamanho - 7;
-    for (let i = tamanho; i >= 1; i--) {
-        soma += parseInt(numeros.charAt(tamanho - i)) * pos--;
-        if (pos < 2) pos = 9;
-    }
-    let resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-    if (resultado !== parseInt(digitos.charAt(0))) return false;
+    const firstDigit = calculateDigit(cnpj.substring(0, 12), 5);
+    if (firstDigit !== parseInt(cnpj.charAt(12))) return false;
 
-    tamanho += 1;
-    numeros = cnpj.substring(0, tamanho);
-    soma = 0;
-    pos = tamanho - 7;
-    for (let i = tamanho; i >= 1; i--) {
-        soma += parseInt(numeros.charAt(tamanho - i)) * pos--;
-        if (pos < 2) pos = 9;
-    }
-    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-    if (resultado !== parseInt(digitos.charAt(1))) return false;
-
-    return true;
+    const secondDigit = calculateDigit(cnpj.substring(0, 13), 6);
+    return secondDigit === parseInt(cnpj.charAt(13));
 };
 
 export const identificarEGuardarCPFouCNPJ = (input: string): void => {
     const cpfOuCnpj = input.replace(/[^\d]+/g, '');
 
-    if (cpfOuCnpj.length === 11 && validarCPF(cpfOuCnpj)) {
+    if (cpfOuCnpj.length === CPF_LENGTH && validarCPF(cpfOuCnpj)) {
         console.log('CPF válido:', cpfOuCnpj);
-        
-    } else if (cpfOuCnpj.length === 14 && validarCNPJ(cpfOuCnpj)) {
+    } else if (cpfOuCnpj.length === CNPJ_LENGTH && validarCNPJ(cpfOuCnpj)) {
         console.log('CNPJ válido:', cpfOuCnpj);
-        
     } else {
         console.log('Número inválido');
     }
