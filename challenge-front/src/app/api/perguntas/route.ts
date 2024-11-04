107ef320243c56a0d36b10a7ba53e22677c7bec3
mateuscastronappe <teteucastro31@gmail.com>
2024-11-03 22:30:28 -0300

    tirando error do catch

--- challenge-front/src/app/api/perguntas/route.ts
+++ challenge-front/src/app/api/perguntas/route.ts
@@ -6,7 +6,7 @@ const bancoPath = path.join(process.cwd(), 'src', 'data', 'banco.json');
 
 export async function POST(request: Request) {
     try {
-        const { pergunta } = await request.json(); 
+        const { pergunta } = await request.json();
         
         const data = JSON.parse(fs.readFileSync(bancoPath, 'utf-8'));
 
@@ -15,10 +15,10 @@ export async function POST(request: Request) {
         }
 
         data.perguntas.push(pergunta);
-        fs.writeFileSync(bancoPath, JSON.stringify(data, null, 2)); 
+        fs.writeFileSync(bancoPath, JSON.stringify(data, null, 2));
 
         return NextResponse.json({ message: 'Pergunta enviada com sucesso!' }, { status: 200 });
-    } catch (error) {
+    } catch {
         return NextResponse.json({ message: 'Erro ao salvar a pergunta.' }, { status: 500 });
     }
 }
@@ -27,7 +27,7 @@ export async function GET() {
     try {
         const data = JSON.parse(fs.readFileSync(bancoPath, 'utf-8'));
         return NextResponse.json(data, { status: 200 });
-    } catch (error) {
+    } catch {
         return NextResponse.json({ message: 'Erro ao carregar as perguntas.' }, { status: 500 });
     }
 }
